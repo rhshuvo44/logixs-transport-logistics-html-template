4bfa03120d55f7b73842b0649aee8173dac59431
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-12-29 12:35:31 +0600

    work card add

--- js/apps.js
+++ js/apps.js
@@ -1,5 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
   freeMode: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
 });
